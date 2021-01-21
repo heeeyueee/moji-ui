8cdc52ca244b40956e4a13f24cdb6e49641ba1d7
heeeyueee <heeeyueee@163.com>
2021-01-21 15:56:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Mojii/>
+</template>
+
+<script>
+import Mojii from './components/Mojii.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Mojii
+  }
+}
+</script>
