cc7f18458f41c3a7efb8fb709116c0ad8a69ec51
heeeyueee <heeeyueee@163.com>
2021-01-25 09:20:20 +0800

    Switch init

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,7 +1,7 @@
 <!-- 组件说明 -->
 <template>
     <div class=''>
-        Swicth
+        <button><span></span></button>
     </div>
 </template>
 
@@ -27,5 +27,27 @@
 
 <style lang='scss' scoped>
 //@import url()
+@mixin borderTest{
+    border:1px red solid;
+}
+$h:22px;
+$h2:$h - 4px;
+button{
+    border:none;
+    height: $h;
+    width:$h*2;
+    border-radius: $h/2;
+    background-color: blue;
+    position: relative;
+}
+span{
+    position: absolute;
+    top:2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    border-radius: $h2/2;
+    background-color: white;
+}
 
 </style>
