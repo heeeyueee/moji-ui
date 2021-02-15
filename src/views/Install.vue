0df6ce4291b88062ba8c3c1cf914ede8b0d01df3
heeeyueee <heeeyueee@163.com>
2021-02-15 15:16:44 +0800

    展示switch源代码

---
+++ src/views/Install.vue
@@ -0,0 +1,22 @@
+<!-- 组件说明 -->
+<template>
+  <article class="markdown-body" v-html="md"></article>
+</template>
+
+<script>
+import md from "../markdown/install.md";
+export default {
+  components: {},
+  data() {
+    return {
+      md,
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+//@import url()
+</style>
