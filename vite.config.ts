b14cc5b0abbf823c56c1d7e9e958a2328e45ed92
heeeyueee <heeeyueee@163.com>
2021-02-18 21:25:06 +0800

    修改配置发布Github

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck
 import { md } from "./plugins/md";
 import fs from 'fs'
-import {baseParse} from '@vue/compiler-core'
+import { baseParse } from '@vue/compiler-core'
 
 export default {
+  base: './',
+  assetsDir: 'assets',
   plugins: [md()],
   vueCustomBlockTransforms: {
     demo: (options) => {
@@ -13,8 +15,7 @@ export default {
       const title = parsed.children[0].content
       const main = file.split(parsed.loc.source).join('').trim()
       return `export default function (Component) {
-        Component.__sourceCode = ${
-        JSON.stringify(main)
+        Component.__sourceCode = ${JSON.stringify(main)
         }
         Component.__sourceCodeTitle = ${JSON.stringify(title)}
       }`.trim()
